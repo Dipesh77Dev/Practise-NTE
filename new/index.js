982bd76c7184945a35cb67049910f793fbc0a611
Dipesh77Dev <rajpersonal777@gmail.com>
2022-05-03 23:57:48 +0530

    Some changes - 5

--- new/index.js
+++ new/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require('mongoose');
 const session = require('express-session');
+const unless = require('express-unless');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -35,6 +36,10 @@ app.use((req, res, next) =>{
     next();
 });
 
+// middlewares auth Routes
+const auth = require("./middlewares/auth.js");
+const errors = require("./middlewares/errors.js");
+
 // set template engine 
 app.set('view engine', 'ejs');
 
@@ -48,11 +53,26 @@ require('./models/product.js');
 require('./models/category.js');
 
 // route prefix or calling routes
-
 app.use("", require("./routes/userRoutes.js"));
 app.use("", require("./routes/categoryRoutes.js"));
 app.use("", require("./routes/productRoutes.js"));
 
+// middlewares for authenticated token 
+auth.authenticateToken.unless = unless; // We are adding unless for not checking auth for this 2 page.
+app.use(
+    auth.authenticateToken.unless({
+        path:[
+            { url : "/users/login", methods : ["POST"] },
+            { url : "/users/register", methods : ["POST"] },
+        ],
+    })
+);
+
+app.use("/users", require("./routes/users.routes.js"));
+
+app.use(errors.errorHandler);
+
+// Server Running
 app.listen(PORT, () => {
     console.log(`Server Started at http://localhost:${PORT}`);
 });
